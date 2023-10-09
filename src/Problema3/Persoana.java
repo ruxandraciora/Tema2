0426de8b3bb9abf882c961ef7f7c5449bf17fe76
ruxandraciora <ruxandra.ciora@student.upt.ro>
2023-10-09 14:13:07 +0300

    Initial commit

---
+++ src/Problema3/Persoana.java
@@ -0,0 +1,45 @@
+package Problema3;
+
+import javax.swing.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+import java.util.Locale;
+
+import static java.lang.Integer.parseInt;
+
+public class Persoana {
+    private String nume;
+    private String cnp;
+
+    public Persoana(String nume, String cnp)
+    {
+        this.nume=nume;
+        this.cnp=cnp;
+    }
+
+    public void afisare() throws ParseException {
+        System.out.println("Nume: "+nume);
+        System.out.println("CNP: "+cnp);
+        System.out.println("Varsta: "+getVarsta());
+    }
+
+    public int getVarsta() throws ParseException {
+        String an = cnp.substring(1,3);
+        String data = cnp.substring(1,7);
+        if(2000+(parseInt(an)) > LocalDate.now().getYear())
+            data = "19" + data;
+        else
+            data = "20" + data;
+        String data_format = data.substring(0,4) + "-" + data.substring(4,6) + "-" + data.substring(6,8);
+        System.out.println(data_format);
+        LocalDate data_actuala = LocalDate.now();
+        //SimpleDateFormat newFormat = new SimpleDateFormat("yyyy-mm-dd");
+        //String data_formatata = newFormat.format(data);
+        LocalDate data_nasterii = LocalDate.parse(data);
+        int varsta = Period.between(data_nasterii,data_actuala).getYears();
+        return varsta;
+    }
+}
---
+++ src/Problema4/Main.java
@@ -0,0 +1,65 @@
+package Problema4;
+
+import Problema3.Persoana;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Scanner read = new Scanner(System.in);
+        String nume = new String();
+        String cnp = new String();
+        System.out.println("Introduceti n: ");
+        int n = read.nextInt();
+        Persoana[] pers = new Persoana[n];
+        int ok = 1;
+        int[] nr_control = new int[]{2,7,9,1,4,6,3,5,8,2,7,9};
+        int cifra_control = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Introduceti numele: ");
+            read.nextLine();
+            nume = read.nextLine();
+            System.out.println("Introduceti CNP-ul: ");
+            cnp = read.nextLine();
+            for(int j=0;j<cnp.length()-1;j++)
+            {
+                if(cnp.charAt(j) < 48 || cnp.charAt(j) > 57)
+                    ok=0;
+                cifra_control += (parseInt(String.valueOf(cnp.charAt(j))))* nr_control[j];
+            }
+            cifra_control = cifra_control % 11;
+            if(cifra_control == 10)
+                cifra_control = 1;
+            while(cnp.length() > 13 || cnp.length() < 13 || ok == 0 || (cnp.charAt(0) != '1' &&
+                    cnp.charAt(0) != '2' && cnp.charAt(0) != '5' && cnp.charAt(0) != '6'))
+            {
+                System.out.println("CNP invalid. Incercati din nou: ");
+                cnp = read.nextLine();
+                ok=1;
+                cifra_control = 0;
+                for(int j=0;j<cnp.length();j++)
+                {
+                    if(cnp.charAt(j) < 48 || cnp.charAt(j) > 57)
+                        ok=0;
+                }
+                for(int j=0;j<cnp.length();j++)
+                {
+                    if(cnp.charAt(j) < 48 || cnp.charAt(j) > 57)
+                        ok=0;
+                    cifra_control += (parseInt(String.valueOf(cnp.charAt(j))))* nr_control[j];
+                }
+                cifra_control = cifra_control % 11;
+                if(cifra_control == 10)
+                    cifra_control = 1;
+            }
+            pers[i] = new Persoana(nume,cnp);
+            pers[i].afisare();
+        }
+    }
+}
